6c35e4b108b60a9ecda56017283ad47d3556b0f8
aca <acadx0@gmail.com>
2020-10-25 01:10:49 +0900

    bypass download limit

--- install.sh
+++ install.sh
@@ -7,12 +7,15 @@ version=$(curl -s 'https://marketplace.visualstudio.com/items?itemName=ms-python
 
 mkdir pkg || true
 cd pkg
+
+curl -c cookie-jar.txt "https://marketplace.visualstudio.com/items?itemName=ms-python.vscode-pylance"
+
 curl -s "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ms-python/vsextensions/vscode-pylance/$version/vspackage" \
   -H 'Upgrade-Insecure-Requests: 1' \
   -H 'DNT: 1' \
   -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36' \
   -H 'Referer: https://marketplace.visualstudio.com/items?itemName=ms-python.vscode-pylance' \
-  --compressed --output ms-python.vscode-pylance-$version
+  -j -b cookie-jar.txt --compressed --output ms-python.vscode-pylance-$version
 
 unzip ms-python.vscode-pylance-$version
 
