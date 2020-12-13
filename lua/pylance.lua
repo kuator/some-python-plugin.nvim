ae62105f27ca891eed8209689e02e6f6bf110fb0
aca <acadx0@gmail.com>
2020-12-14 01:17:35 +0900

    remove before_init

--- lua/pylance.lua
+++ lua/pylance.lua
@@ -1,25 +1,20 @@
-local configs = require("nvim_lsp/configs")
+local configs = require("lspconfig/configs")
+local util = require 'lspconfig/util'
+
 local _, _, plug_path = string.find(debug.getinfo(1, "S").source:sub(2),  "(.*/)lua/pylance.lua")
 
 configs.pylance = {
     default_config = {
         cmd = {"node", plug_path .. "pkg/extension/dist/server.bundle.js", "--stdio"},
         filetypes = {"python"},
-        root_dir = function()
-            return vim.loop.cwd()
-        end,
-        -- https://github.com/microsoft/pylance-release#settings-and-customization
+        root_dir = util.root_pattern(".git", "setup.py",  "setup.cfg", "pyproject.toml", "requirements.txt");
         settings = {
-        },
-        -- TODO: remove this
-        -- https://github.com/neovim/nvim-lspconfig/issues/299
-        before_init = function(initialize_params, config)
-            initialize_params["workspaceFolders"] = {
-                {
-                    name = "workspace",
-                    uri = initialize_params["rootUri"]
-                }
-            }
-        end
+          python = {
+            analysis = {
+              autoSearchPaths = true;
+              useLibraryCodeForTypes = true;
+            };
+          };
+        };
     }
 }
