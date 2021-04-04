3c2f7e224b2ed87aebe1d1ddacb0fbb2df1e64c0
evakuator <kuatabylkasymov@gmail.com>
2021-04-04 15:19:27 +0600

    fix path to pylance

--- lua/pylance.lua
+++ lua/pylance.lua
@@ -5,11 +5,11 @@ local util = require 'lspconfig/util'
 vim.cmd [[ let $ELECTRON_RUN_AS_NODE = 1 ]]
 vim.cmd [[ let $VSCODE_NLS_CONFIG = '{"locale": "en"}' ]]
 
-local _, _, plug_path = string.find(debug.getinfo(1, "S").source:sub(2),  "(.*/)lua/pylance.lua")
+lua pylance_home=vim.fn.system('find ~/.vscode/extensions -iname "*pylance*" -type d | sort | tail -1')
 
 configs.pylance = {
     default_config = {
-        cmd = {"node", plug_path .. "pkg/extension/dist/server.bundle.js", "--stdio"},
+        cmd = {"node", pylance_home .. "/dist/server.bundle.js", "--stdio"},
         filetypes = {"python"},
         root_dir = util.root_pattern(".git", "setup.py",  "setup.cfg", "pyproject.toml", "requirements.txt");
         settings = {
