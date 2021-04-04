8832f39b3f414a0cd679d46182a658b4020fa08a
evakuator <kuatabylkasymov@gmail.com>
2021-04-04 15:25:36 +0600

    small fix

--- lua/pylance.lua
+++ lua/pylance.lua
@@ -5,7 +5,9 @@ local util = require 'lspconfig/util'
 vim.cmd [[ let $ELECTRON_RUN_AS_NODE = 1 ]]
 vim.cmd [[ let $VSCODE_NLS_CONFIG = '{"locale": "en"}' ]]
 
-pylance_home=vim.fn.system('find ~/.vscode/extensions -iname "*pylance*" -type d | sort | tail -1')
+local pylance_home=vim.fn.system('find ~/.vscode/extensions -iname "*pylance*" -type d | sort | tail -1')
+pylance_home = pylance_home:gsub("%s+", "")
+
 
 configs.pylance = {
     default_config = {
