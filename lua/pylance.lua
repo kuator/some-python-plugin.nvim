fd58b44f6983b88341dd501c0ad25389171b910b
evakuator <kuatabylkasymov@gmail.com>
2022-05-26 12:15:58 +0600

    update

--- lua/pylance.lua
+++ lua/pylance.lua
@@ -5,12 +5,13 @@ local util = require 'lspconfig.util'
 vim.cmd [[ let $ELECTRON_RUN_AS_NODE = 1 ]]
 vim.cmd [[ let $VSCODE_NLS_CONFIG = '{"locale": "en"}' ]]
 
-local pylance_home=vim.fn.system('find ~/opt/pkg -iname "*pylance*" -type d | sort | tail -1')
-pylance_home = pylance_home:gsub("%s+", "")
+-- local pylance_home=vim.fn.system('find ~/opt/pkg -iname "*pylance*" -type d | sort | tail -1')
+-- pylance_home = pylance_home:gsub("%s+", "")
 
 configs.pylance = {
     default_config = {
-        cmd = {"node", pylance_home .. "/dist/server.bundle.js", "--stdio"},
+        -- cmd = {"node", pylance_home .. "/dist/server.bundle.js", "--stdio"},
+        cmd = {"pylance"},
         filetypes = {"python"},
         root_dir = util.root_pattern(".git", "setup.py",  "setup.cfg", "pyproject.toml", "requirements.txt");
         settings = {
